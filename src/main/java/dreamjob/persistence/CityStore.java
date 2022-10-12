2db4702253ebd4918570d455b6ac304b505b0490
svoh86 <sms-86@mail.ru>
2022-10-12 12:38:00 +0300

    2. Формы. Списки. [#504854 #350716]

---
+++ src/main/java/dreamjob/persistence/CityStore.java
@@ -0,0 +1,39 @@
+package dreamjob.persistence;
+
+import dreamjob.model.City;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+@ThreadSafe
+@Repository
+public class CityStore {
+    private final Map<Integer, City> cities = new HashMap<>();
+
+    public CityStore() {
+        cities.put(1, new City(1, "Москва"));
+        cities.put(2, new City(2, "Санкт-Петербург"));
+        cities.put(3, new City(3, "Екатеринбург"));
+        cities.put(4, new City(4, "Новосибирск"));
+        cities.put(5, new City(5, "Сочи"));
+        cities.put(6, new City(6, "Владивосток"));
+        cities.put(7, new City(7, "Хабаровск"));
+    }
+
+    public List<City> getAllCities() {
+        return new ArrayList<>(cities.values());
+    }
+
+    public City findById(int id) {
+        return cities.get(id);
+    }
+}
--- src/main/java/dreamjob/service/CityService.java
+++ src/main/java/dreamjob/service/CityService.java
@@ -1,38 +1,32 @@
 package dreamjob.service;
 
 import dreamjob.model.City;
+import dreamjob.persistence.CityStore;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
+ * Слой service
+ *
  * @author Svistunov Mikhail
  * @version 1.0
  */
 @ThreadSafe
 @Service
 public class CityService {
-    private final Map<Integer, City> cities = new HashMap<>();
+    private final CityStore cityStore;
 
-    public CityService() {
-        cities.put(1, new City(1, "Москва"));
-        cities.put(2, new City(2, "Санкт-Петербург"));
-        cities.put(3, new City(3, "Екатеринбург"));
-        cities.put(4, new City(4, "Новосибирск"));
-        cities.put(5, new City(5, "Сочи"));
-        cities.put(6, new City(6, "Владивосток"));
-        cities.put(7, new City(7, "Хабаровск"));
+    public CityService(CityStore cityStore) {
+        this.cityStore = cityStore;
     }
 
     public List<City> getAllCities() {
-        return new ArrayList<>(cities.values());
+        return cityStore.getAllCities();
     }
 
     public City findById(int id) {
-        return cities.get(id);
+        return cityStore.findById(id);
     }
 }
