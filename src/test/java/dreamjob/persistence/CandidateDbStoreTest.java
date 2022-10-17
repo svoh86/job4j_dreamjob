56c17f1987efcfbfaa7a8e6f1e3fda537ea21ffb
svoh86 <sms-86@mail.ru>
2022-10-17 09:44:19 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862 #351642]

---
+++ src/test/java/dreamjob/persistence/CandidateDbStoreTest.java
@@ -0,0 +1,40 @@
+package dreamjob.persistence;
+
+import dreamjob.Main;
+import dreamjob.model.Candidate;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CandidateDbStoreTest {
+
+    @Test
+    public void whenCreateCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan", "Java Dev", LocalDateTime.now(), null);
+        store.add(candidate);
+        Candidate candidateInDb = store.findById(candidate.getId());
+        assertThat(candidateInDb.getName()).isEqualTo(candidate.getName());
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "Ivan", "Java Dev", LocalDateTime.now(), null);
+        store.add(candidate);
+        store.update(new Candidate(1, "Petr", "Java Dev", LocalDateTime.now(), null));
+        Candidate candidateInDb = store.findById(candidate.getId());
+        assertThat(candidateInDb.getName()).isEqualTo(candidate.getName());
+    }
+
+    @Test
+    public void whenFindByIdCandidate() {
+        CandidateDbStore store = new CandidateDbStore(new Main().loadPool());
+        Candidate candidate = new Candidate(10, "Ivan", "Java Dev", LocalDateTime.now(), null);
+        store.add(candidate);
+        Candidate candidateInDb = store.findById(candidate.getId());
+        assertThat(candidateInDb.getId()).isEqualTo(candidate.getId());
+    }
+}
--- src/test/java/dreamjob/persistence/PostDbStoreTest.java
+++ src/test/java/dreamjob/persistence/PostDbStoreTest.java
@@ -1,8 +1,11 @@
 package dreamjob.persistence;
 
 import dreamjob.Main;
+import dreamjob.model.City;
 import dreamjob.model.Post;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,9 +14,28 @@ class PostDbStoreTest {
     @Test
     public void whenCreatePost() {
         PostDbStore store = new PostDbStore(new Main().loadPool());
-        Post post = new Post(0, "Java Job");
+        Post post = new Post(0, "Java Job", "Dev", LocalDateTime.now(), true, new City());
         store.add(post);
         Post postInDb = store.findById(post.getId());
         assertThat(postInDb.getName()).isEqualTo(post.getName());
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job", "Dev", LocalDateTime.now(), true, new City());
+        store.add(post);
+        store.update(new Post(1, "Junior Job", "Dev", LocalDateTime.now(), false, new City()));
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName()).isEqualTo(post.getName());
+    }
+
+    @Test
+    public void whenFindByIdPost() {
+        PostDbStore store = new PostDbStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job", "Dev", LocalDateTime.now(), true, new City());
+        store.add(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getId()).isEqualTo(post.getId());
     }
 }
